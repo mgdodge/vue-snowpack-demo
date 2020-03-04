fc891b78b5e87c506d29d57c8b9756b5f7cf64b1
Michael Dodge <michael.dodge@vivint.com>
2020-03-04 09:30:03 -0600

    Finalize demo code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DeepThought from './components/DeepThought.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    DeepThought,
+  },
+  data() {
+    return {
+      who: '',
+    };
+  },
 }
 </script>
 
+<template>
+  <div id="app">
+    <h1 v-if="!who">Hello, I am Deep Thought. Who is this?</h1>
+    <h1 v-else>Greetings {{ who }}.</h1>
+    <DeepThought v-model="who" />
+  </div>
+</template>
+
 <style>
+html, body {
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background: url('/assets/deep-thought.jpg') no-repeat top center;
+  background-size: cover;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
---
+++ src/components/DeepThought.vue
@@ -0,0 +1,60 @@
+<script>
+import { answer, question } from 'the-ultimate-question';
+export default {
+  name: 'DeepThought',
+  props: {
+    value: String
+  },
+  data() {
+    return {
+      answer: answer(),
+      question: question(),
+      asked: false,
+    };
+  },
+  methods: {
+    buildComputer() {
+      this.asked = true;
+    },
+    reset() {
+      console.log('Alright, I\'ve forgotten everything.');
+      this.asked = false;
+      this.$emit('input', '');
+    },
+  },
+}
+</script>
+
+<template>
+  <div v-if="!value">
+    <button @click="$emit('input', 'Zephod')">Zephod Beeblebrox</button>
+    <button @click="$emit('input', 'Arthur')">Arthur Dent</button>
+    <button @click="$emit('input', 'Trillian')">Tricia McMillan</button>
+  </div>
+  <div v-else-if="!asked">
+    The answer is {{ answer }}. I'm quite sure it's right.<br>
+    I can build a computer to determine the question...<br><br>
+    <button @click="buildComputer">Do it!</button>
+  </div>
+  <div v-else class="question">
+    {{ question }}<br><br>
+    <button @click="reset">Reset</button>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+  background: #ff8200;
+  color: #fff;
+  border: none;
+  padding: 7px 12px;
+  font-weight: bold;
+  border-radius: 4px;
+  margin: 0 5px 5px 0;
+  cursor: pointer;
+}
+.question {
+  max-width: 300px;
+}
+</style>
